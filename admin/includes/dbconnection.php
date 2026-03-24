48dfa91e26c821a3ad34b1512ac02b474a234517
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-24 22:23:59 +0530

    db connection create

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,13 @@
+<?php 
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','pereraservice');
+
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
